d27c96d013d7c97413f9d77983876c0b69b2f46d
Mohanish Mankar <mohanish35@gmail.com>
2025-08-09 14:54:52 +0530

    setup Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
-import type { AppProps } from "next/app";
-import "@/styles/globals.css";
+import type { AppProps } from "next/app"
+import "@/styles/globals.css"
+import { Analytics } from "@vercel/analytics/next"
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log("hehe")
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
